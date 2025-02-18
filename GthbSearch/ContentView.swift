0f9198caece9fc02ab50025e8e4f01626d9caf1f
Holló Balázs <holbalzs@gmail.com>
2025-02-18 10:25:50 +0100

    project init

---
+++ GthbSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GthbSearch
+//
+//  Created by Holló Balázs on 2025. 02. 18..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GthbSearch/GthbSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GthbSearchApp.swift
+//  GthbSearch
+//
+//  Created by Holló Balázs on 2025. 02. 18..
+//
+
+import SwiftUI
+
+@main
+struct GthbSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
